99dec4305986150e5b312642ab796b8d20c8834f
Adrián Abalde <aabalde@gradiant.org>
2018-07-05 16:58:14 +0200

    Update test class

--- java/src/test/java/WeatherKataTest.java
+++ java/src/test/java/WeatherKataTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Date;
 
 import static junit.framework.TestCase.assertTrue;
@@ -12,41 +13,41 @@ public class WeatherKataTest {
 
     // https://www.metaweather.com/api/location/766273/
     @Test
-    public void find_the_weather_of_today() throws IOException {
+    public void find_the_weather_of_today() throws IOException, ParseException {
         Forecast forecast = new Forecast();
 
-        String prediction = forecast.predict("Madrid",null, false);
+        String prediction = forecast.predictWeather("Madrid",null);
 
         System.out.println("Today: " + prediction);
         assertTrue("I don't know how to test it", true);
     }
     @Test
-    public void find_the_weather_of_any_day() throws IOException {
+    public void find_the_weather_of_any_day() throws IOException, ParseException {
         Forecast forecast = new Forecast();
 
         Date tomorrow = new Date(new Date().getTime() + ONE_DAY);
 
-        String prediction = forecast.predict("Madrid",tomorrow, false);
+        String prediction = forecast.predictWeather("Madrid",tomorrow);
         System.out.println("Tomorrow: " + prediction);
         assertTrue("I don't know how to test it", true);
     }
     @Test
-    public void find_the_wind_of_any_day() throws IOException {
+    public void find_the_wind_of_any_day() throws IOException, ParseException {
         Forecast forecast = new Forecast();
 
-        String prediction = forecast.predict("Madrid",null, true);
+        String prediction = forecast.predictWind("Madrid",null);
 
         System.out.println("Wind: " + prediction);
         assertTrue("I don't know how to test it", true);
     }
 
     @Test
-    public void there_is_no_prediction_for_more_than_5_days() throws IOException {
+    public void there_is_no_prediction_for_more_than_5_days() throws IOException, ParseException {
         Forecast forecast = new Forecast();
 
         Date tomorrow = new Date(new Date().getTime() + (ONE_DAY * 6));
 
-        String prediction = forecast.predict("Madrid",tomorrow, false);
+        String prediction = forecast.predictWeather("Madrid",tomorrow);
         assertEquals("", prediction);
     }
 }
