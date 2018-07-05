434934abce337fdf01115de7441ef2dbd485a4d7
Adrián Abalde <aabalde@gradiant.org>
2018-07-05 16:09:05 +0200

    Extract guard clause

--- java/src/main/java/Forecast.java
+++ java/src/main/java/Forecast.java
@@ -24,46 +24,45 @@ public class Forecast {
     private static final String JSON_FIELD_WEATHER = "weather_state_name";
 
     public String predict(String city, Date datetime, boolean wind) throws IOException {
+        // If there are predictions
+        if (!datetime.before(new Date(new Date().getTime() + (ONE_DAY * 6)))) {
+            return "";
+        }
+
         // When date is not provided we look for the current prediction
         if (datetime == null) {
             datetime = new Date();
         }
         String format = new SimpleDateFormat(DATE_FORMAT).format(datetime);
 
-        // If there are predictions
+        // Find the id of the city on metawheather
+        HttpRequestFactory requestFactory
+                = new NetHttpTransport().createRequestFactory();
+        HttpRequest request = requestFactory.buildGetRequest(
+                new GenericUrl(URL_SEARCH_CITY + city));
+        String rawResponse = request.execute().parseAsString();
+        JSONArray jsonArray = new JSONArray(rawResponse);
+        String woeid = jsonArray.getJSONObject(0).get(JSON_FIELD_CITY_ID).toString();
 
-        if (!datetime.before(new Date(new Date().getTime() + (ONE_DAY * 6)))) {
-            return "";
-        } else {
+        // Find the predictions for the city
+        requestFactory = new NetHttpTransport().createRequestFactory();
+        request = requestFactory.buildGetRequest(
+                new GenericUrl(URL_PREDICT_WEATHER + woeid));
+        rawResponse = request.execute().parseAsString();
+        JSONArray results = new JSONObject(rawResponse).getJSONArray(JSON_FIELD_PREDICTION);
 
-            // Find the id of the city on metawheather
-            HttpRequestFactory requestFactory
-                    = new NetHttpTransport().createRequestFactory();
-            HttpRequest request = requestFactory.buildGetRequest(
-                    new GenericUrl(URL_SEARCH_CITY + city));
-            String rawResponse = request.execute().parseAsString();
-            JSONArray jsonArray = new JSONArray(rawResponse);
-            String woeid = jsonArray.getJSONObject(0).get(JSON_FIELD_CITY_ID).toString();
-
-            // Find the predictions for the city
-            requestFactory = new NetHttpTransport().createRequestFactory();
-            request = requestFactory.buildGetRequest(
-                    new GenericUrl(URL_PREDICT_WEATHER + woeid));
-            rawResponse = request.execute().parseAsString();
-            JSONArray results = new JSONObject(rawResponse).getJSONArray(JSON_FIELD_PREDICTION);
-
-            for (int i = 0; i < results.length(); i++) {
+        for (int i = 0; i < results.length(); i++) {
 //            // When the date is the expected
-                if (format.equals(results.getJSONObject(i).get(JSON_FIELD_DATE).toString())) {
+            if (format.equals(results.getJSONObject(i).get(JSON_FIELD_DATE).toString())) {
 //                // If we have to return the wind information
-                    if (wind) {
-                        return results.getJSONObject(i).get(JSON_FIELD_WIND).toString();
-                    } else {
-                        return results.getJSONObject(i).get(JSON_FIELD_WEATHER).toString();
-                    }
+                if (wind) {
+                    return results.getJSONObject(i).get(JSON_FIELD_WIND).toString();
+                } else {
+                    return results.getJSONObject(i).get(JSON_FIELD_WEATHER).toString();
                 }
             }
         }
+
         return "";
     }
 }
