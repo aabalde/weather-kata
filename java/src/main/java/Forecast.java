ee26193b1d69ee3573a3e4cfa6e17934af5316aa
Adrián Abalde <aabalde@gradiant.org>
2018-07-05 17:58:06 +0200

    Forecast with service injection

--- java/src/main/java/Forecast.java
+++ java/src/main/java/Forecast.java
@@ -1,34 +1,21 @@
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Forecast {
-    private int ONE_DAY = 1000 * 60 * 60 * 24;
 
-    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private IForecast service;
 
-    private static final String URL_SEARCH_CITY = "https://www.metaweather.com/api/location/search/?query=";
-    private static final String URL_PREDICT_WEATHER = "https://www.metaweather.com/api/location/";
+    public Forecast(){
+        this.service = new HttpForecast();
+    }
 
-    private static final String JSON_FIELD_CITY_ID = "woeid";
-    private static final String JSON_FIELD_PREDICTION = "consolidated_weather";
-    private static final String JSON_FIELD_DATE = "applicable_date";
-    private static final String JSON_FIELD_WIND = "wind_speed";
-    private static final String JSON_FIELD_WEATHER = "weather_state_name";
+    public Forecast(IForecast service){
+        this.service = service;
+    }
 
-    private SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
-
-    
     public String predictWeather(String city, Date datetime) throws Exception {
-        Prediction prediction = getPrediction(city, datetime);
+        Date date = checkDate(datetime);
+        Prediction prediction = this.service.predict(city, date);
         if(prediction == null){
             return "";
         }
@@ -39,7 +26,8 @@ public class Forecast {
 
 
     public String predictWind(String city, Date datetime) throws Exception {
-        Prediction prediction = getPrediction(city, datetime);
+        Date date = checkDate(datetime);
+        Prediction prediction = this.service.predict(city, date);
         if(prediction == null){
             return "";
         }
@@ -48,69 +36,11 @@ public class Forecast {
         return prediction.getWind();
     }
 
-
-    private Prediction getPrediction(String city, Date datetime) throws Exception {
-        Date date = checkDate(datetime);
-        if (!isPredictionAvailable(date)) {
-            return null;
-        }
-
-        String cityId = getCityId(city);
-        JSONArray predictions = getOneWeekPredictions(cityId);
-        JSONObject predictionJson = getPrediction(date, predictions);
-
-        Prediction prediction = new Prediction(predictionJson.get(JSON_FIELD_WEATHER).toString(),
-                predictionJson.get(JSON_FIELD_WIND).toString());
-
-        return prediction;
-    }
-
-
-    private JSONObject getPrediction(Date date, JSONArray predictions) throws ParseException {
-        JSONObject prediction = null;
-        for (int i = 0; i < predictions.length(); i++) {
-            JSONObject currentPrediction = predictions.getJSONObject(i);
-            String currentDate = currentPrediction.get(JSON_FIELD_DATE).toString();
-            if (date.equals(format.parse(currentDate))) {
-                prediction = currentPrediction;
-                break;
-            }
-        }
-        return prediction;
-    }
-
-
-    private JSONArray getOneWeekPredictions(String cityId) throws IOException {
-        HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
-        HttpRequest request = requestFactory.buildGetRequest(
-                new GenericUrl(URL_PREDICT_WEATHER + cityId));
-        String rawResponse = request.execute().parseAsString();
-        return new JSONObject(rawResponse).getJSONArray(JSON_FIELD_PREDICTION);
-    }
-
-
-    private String getCityId(String city) throws IOException {
-        HttpRequestFactory requestFactory
-                = new NetHttpTransport().createRequestFactory();
-        HttpRequest request = requestFactory.buildGetRequest(
-                new GenericUrl(URL_SEARCH_CITY + city));
-        String rawResponse = request.execute().parseAsString();
-        JSONArray jsonArray = new JSONArray(rawResponse);
-        return jsonArray.getJSONObject(0).get(JSON_FIELD_CITY_ID).toString();
-    }
-
-
-    private boolean isPredictionAvailable(Date datetime) throws Exception{
-        long today = new Date().getTime();
-        Date oneWeekLater = format.parse(format.format(new Date(today + (ONE_DAY * 6))));
-        return datetime.before(oneWeekLater);
-    }
-
-
-    private Date checkDate(Date datetime) throws ParseException {
+    private Date checkDate(Date datetime) {
+        Date date = null;
         if (datetime == null) {
-            datetime = new Date();
+            date = new Date();
         }
-        return format.parse(format.format(datetime));
+        return date;
     }
 }
