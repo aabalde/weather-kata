f7dab26b66f90043d70f9c56d28ee2ea0c65f9e1
Adrián Abalde <aabalde@gradiant.org>
2018-07-06 09:07:25 +0200

    Inject service stub test today

--- java/src/test/java/WeatherKataTest.java
+++ java/src/test/java/WeatherKataTest.java
@@ -1,11 +1,21 @@
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
 
 public class WeatherKataTest {
 
@@ -13,14 +23,41 @@ public class WeatherKataTest {
 
     // https://www.metaweather.com/api/location/766273/
     @Test
-    public void find_the_weather_of_today() throws Exception {
-        Forecast forecast = new Forecast();
+    public void find_the_weather_of_today_returns_sunny() throws Exception {
+        String expectedWeather = "Sunny";
+        String expectedWind = "4.5";
+        Prediction expectedPrediction = new Prediction(expectedWeather,expectedWind);
 
-        String prediction = forecast.predictWeather("Madrid",null);
+        IForecast serviceStub = Mockito.mock(IForecast.class);
+        when(serviceStub.predict(eq("Madrid"), any(Date.class))).thenAnswer(new Answer<Prediction>() {
+            @Override
+            public Prediction answer(InvocationOnMock invocation) throws Throwable {
+                Object[] args = invocation.getArguments();
 
-        System.out.println("Today: " + prediction);
-        assertTrue("I don't know how to test it", true);
+                Date date = (Date) args[1];
+                Date today = new Date();
+
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(date);
+                Calendar calToday = Calendar.getInstance();
+                calToday.setTime(today);
+
+                if(cal.get(Calendar.YEAR) == calToday.get(Calendar.YEAR)
+                        && cal.get(Calendar.MONTH) == calToday.get(Calendar.MONTH)
+                        && cal.get(Calendar.DAY_OF_MONTH) == calToday.get(Calendar.DAY_OF_MONTH)){
+                    return expectedPrediction;
+                } else {
+                    throw new ForecastException("The date received must be today");
+                }
+            }
+        });
+
+        Forecast forecast = new Forecast(serviceStub);
+        String predictedWeather = forecast.predictWeather("Madrid",null);
+
+        assertEquals(expectedWeather, predictedWeather);
     }
+
     @Test
     public void find_the_weather_of_any_day() throws Exception {
         Forecast forecast = new Forecast();
@@ -41,13 +78,12 @@ public class WeatherKataTest {
         assertTrue("I don't know how to test it", true);
     }
 
-    @Test
+    @Test(expected = ForecastException.class)
     public void there_is_no_prediction_for_more_than_5_days() throws Exception {
         Forecast forecast = new Forecast();
 
         Date tomorrow = new Date(new Date().getTime() + (ONE_DAY * 6));
 
-        String prediction = forecast.predictWeather("Madrid",tomorrow);
-        assertEquals("", prediction);
+        forecast.predictWeather("Madrid",tomorrow);
     }
 }
